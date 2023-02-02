665e763f4cc608c47550f6b35218572dd06d0e26
Anthony <ajackerm@mtu.edu>
2023-02-01 20:56:23 -0500

    Fix/CSS Battery Profile
    Added header bar and fix CSS of form, Battery prof needs notes saying to add commas

--- app/EES_Forms/forms.py
+++ app/EES_Forms/forms.py
@@ -181,9 +181,10 @@ class daily_battery_profile_form(ModelForm):
         fields = ('foreman', 'crew', 'inop_ovens', 'inop_numbs', 'facility')
         
         widgets = {
+            'foreman': forms.TextInput(attrs={'type': 'text', 'placeholder': 'Last Name', 'class': 'input', 'style': 'width:120px; text-align:center;'}),
             'facility': forms.TextInput(attrs={'type': 'hidden'}),
             'inop_ovens': forms.NumberInput(attrs={'class': 'input', 'type': 'number', 'style': 'width:50px;'}),
-            'inop_numbs': forms.TextInput(attrs={'class': 'input', 'style': 'width:150px;'})
+            'inop_numbs': forms.TextInput(attrs={'class': 'input', 'placeholder': 'Ex. 1,2,3,45', 'style': 'width:150px; text-align:center;'})
         }
 
 
