1fbc821684bd1f303c5b4c8558bad1a67dd373d9
Anthony <ajackerm@mtu.edu>
2023-02-01 22:28:37 -0500

    Fixed Form H
    - Form h ambient temp stop is listed as tbd

--- app/EES_Forms/views/formH.py
+++ app/EES_Forms/views/formH.py
@@ -294,14 +294,16 @@ def formH(request, facility, selector):
                 B = readings.save(commit=False)
                 A.facilityChoice = options
                 if not existing:
-                    if int(A.wind_speed_start) == int(round(city_weather['wind']['speed'], 0)):
-                        A.wind_speed_stop = 'same'
-                    else:
-                        A.wind_speed_stop = round(city_weather['wind']['speed'], 0)
-                    if int(A.ambient_temp_start) == int(round(city_weather['main']['temp'], 0)):
-                        A.wind_temp_stop = 'same'
-                    else:
-                        A.ambient_temp_stop = round(city_weather['main']['temp'], 0)
+                    if A.wind_speed_stop == 'TBD':
+                        if int(A.wind_speed_start) == int(round(city_weather['wind']['speed'], 0)):
+                            A.wind_speed_stop = 'same'
+                        else:
+                            A.wind_speed_stop = round(city_weather['wind']['speed'], 0)
+                    if A.ambient_temp_stop == 'TBD':
+                        if int(A.ambient_temp_start) == int(round(city_weather['main']['temp'], 0)):
+                            A.ambient_temp_stop = 'same'
+                        else:
+                            A.ambient_temp_stop = round(city_weather['main']['temp'], 0)
                         
                 A.save()
 
