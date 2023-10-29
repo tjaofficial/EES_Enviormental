48efa026783dd1a7ee3f6aaefb66d2ede47a6277
Anthony <ajackerm@mtu.edu>
2023-10-29 17:53:28 -0400

    Updating S3 uploads

--- app/EES_Forms/models.py
+++ app/EES_Forms/models.py
@@ -6873,7 +6873,7 @@ class sop_model(models.Model):
         auto_now=False,
         blank=True,
     )
-    pdf_file = models.FileField(upload_to='media/SOPs/')
+    pdf_file = models.FileField(upload_to='SOPs/')
     pdf_url = models.CharField(
         max_length=1000000,
         blank=True
