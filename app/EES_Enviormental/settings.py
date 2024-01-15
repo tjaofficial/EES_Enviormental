3a32992e52eb9c1ddd6df5526524edc539ec4598
Micah <mtrierweiler@voxoluniverse.com>
2024-01-15 07:44:42 -0500

    Pre push tobes last changes

--- app/EES_Enviormental/settings.py
+++ app/EES_Enviormental/settings.py
@@ -199,11 +199,11 @@ CHANNEL_LAYERS = {
 
 #EMAIL
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_POST = os.getenv('EMAIL_POST')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = 'qkcufopwjyavcpqn'
 
 
 
