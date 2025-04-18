bc9a2bff08020e440f4d0c96d7dcea4d14a3c841
Anthony <ajackerm@mtu.edu>
2025-04-17 22:43:43 -0400

    Update to daphne

--- app/EES_Enviormental/asgi.py
+++ app/EES_Enviormental/asgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from django.core.asgi import get_asgi_application # type: ignore
+from channels.routing import ProtocolTypeRouter, URLRouter # type: ignore
+from channels.auth import AuthMiddlewareStack # type: ignore
 import EES_Forms.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EES_Enviormental.settings')
@@ -22,7 +22,6 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     'http':get_asgi_application(),
-    # 'websocket': URLRouter(urls.websocket_urlpatterns)
     'websocket': AuthMiddlewareStack(
         URLRouter(
             EES_Forms.routing.websocket_urlpatterns
--- app/EES_Forms/routing.py
+++ app/EES_Forms/routing.py
@@ -1,6 +1,6 @@
-from django.urls import re_path
+from django.urls import re_path # type: ignore
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/notifications/(?P<facility>\w+)/$', consumers.NotifConsumer.as_asgi())
+    re_path(r"ws/notifications/(?P<facility>\w+)/$", consumers.NotifConsumer.as_asgi())
 ]
