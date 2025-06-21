1e0735632b203856086e9b093db2b7affcd53b42
Anthony <ajackerm@mtu.edu>
2025-06-20 22:57:23 -0400

    Upudate initial data set

--- app/EES_Forms/views/inital_form_add.py
+++ app/EES_Forms/views/inital_form_add.py
@@ -34,3 +34,37 @@ def add_forms_to_database():
 
     if Forms.objects.count() <= 5:
         create_starting_forms()
+    
+    if not braintreePlans.objects.all().exists():
+        newPlan1 = braintreePlans(
+            planID='prod_S7vHowgpKXFyoW',
+            priceID='price_1RDfQb4SO9L4wW3IuYiT5MVk',
+            name='MethodPro+',
+            price=250.0,
+            description='Registration for one (1) supervisor and one (1) observer.'
+        )
+        newPlan2 = braintreePlans(
+            planID='prod_S7vHNl3m2si6f3',
+            priceID='price_1RDfQz4SO9L4wW3Ihw4yqNE9',
+            name='MethodPremium+',
+            price=300.0,
+            description='Registration for one (1) supervisor and one (1) observer.'
+        )
+        newPlan3 = braintreePlans(
+            planID='prod_S7v5DsOU0LUxM0',
+            priceID='price_1RDfFM4SO9L4wW3IBTyiv1nE',
+            name='MethodPlus+',
+            price=199.0,
+            description='Registration for one (1) supervisor and one (1) observer.'
+        )
+        test = braintreePlans(
+            planID='prod_SLIU6peYFES7MA',
+            priceID='price_1RQbtAG3aFwevvRDl3drrctk',
+            name='Test',
+            price=2.0,
+            description='test'
+        )
+        newPlan1.save()
+        newPlan2.save()
+        newPlan3.save()
+        test.save()
